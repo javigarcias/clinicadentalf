5aefb29ae19f226018c48fab053986d142f3a06f
javigarcia <javigarciaweb@gmail.com>
2020-10-24 10:46:10 +0200

    Added views appoiments in Perfil container

--- src/containers/Perfil/Perfil.jsx
+++ src/containers/Perfil/Perfil.jsx
@@ -1,47 +1,78 @@
-import Axios from 'axios';
-import React, { useState } from 'react'
-import { Redirect, useHistory } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Perfil = () => {
-    const clientelogeado = localStorage.getItem('user');
+    const clientelogeado = JSON.parse( localStorage.getItem('user'));
     const history = useHistory();
-    const [cita, setCita] = useState({
-        fecha:"",
-        tratamiento:""
-    });
+    const [cita, setCita] = useState();
+    const [citaCreada, setCitaCreada] = useState();
+    const [datosCitas, setDatosCitas] = useState([]);
+    
 
-    if(!clientelogeado) return <Redirect to='/'/>
+    //if(!clientelogeado) {return <Redirect to='/'/>}
+
+    useEffect(() => {
+
+        /* const options = {
+            params: {token: clientelogeado.token}}
+        console.log(options) */
+        axios.get('http://localhost:3001/citas/ver/' +clientelogeado.token)
+        .then( (res) => {
+            setDatosCitas(res.data.citas);
+            console.log (res.data.citas);
+
+        }).catch( (err) => {
+            console.log( err );
+
+        });
+    },[]);
+
 
     const manejaCita = ev => {
         setCita({...cita, [ev.target.name] : ev.target.value});
     };
-
+    
     const creaCita = () => {
+        console.log(clientelogeado)
         let citaBody = {
             fecha : cita.fecha,
+            email : clientelogeado.email,
             tratamiento : cita.tratamiento,
-            ideuser : clientelogeado._id,
-            estado : "pendiente"  
+            iduser : clientelogeado.token
         };
-        Axios.post('http://localhost:3001/citas/nuevacita', citaBody)
+        axios.post('http://localhost:3001/citas/nuevacita', citaBody);
+        setCitaCreada(citaBody);
     }
-
-
 
     const logout = ev => {
         localStorage.clear();
         history.push("/")
+    
     }
+    
     return (
         <div className="profile">
-            <h2>VISTA PERFIL</h2>
+            <h2>Bienvenido {clientelogeado.nombre}</h2>
+            <div>
             <h3>Nueva Cita</h3>
             <h4>Fecha</h4>
             <input type="text" placeholder="DD/MM/AAAA" name="fecha" onChange={manejaCita}></input>
             <h4>Tratamiento</h4>
             <input type="text" placeholder="Tratamiento" name="tratamiento" onChange={manejaCita}></input>
+            </div>
             <button onClick={() => {creaCita()}} >Crear cita</button>
             <button onClick={logout}>Logout</button>
+            <div>
+                {citaCreada?.fecha}
+                {citaCreada?.tratamiento}
+
+
+                
+            </div>
+            <div>
+            {datosCitas?.map(cita => <div className="cardCitas" key={cita._id} >{cita.fecha}</div>)}            </div>
+
         </div>
     )
 }
